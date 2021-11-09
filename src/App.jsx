5a1d31167b3d392be4e7815a97879c1f7baeddee
badal <badal1906@gmail.com>
2021-11-09 11:57:07 +0530

    login page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="app">
+      {/* spotify logo */}
+      {/* login with spotify button */}
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
